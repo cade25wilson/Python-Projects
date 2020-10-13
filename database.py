75964ca49e17acc3243c113ff6a0ac0c9820563c
Cade Wilson <cade25wilson@gmail.com>
2020-10-13 12:56:53 -0600

    Update database.py
    remove hardcoding of .txt

--- database.py
+++ database.py
@@ -1,31 +1,28 @@
+
 import sqlite3
 
-conn = sqlite3.connect('test3.db')
+conn = sqlite3.connect('test2.db')
 
 with conn:
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS tbl_files( \
         ID INTEGER PRIMARY KEY AUTOINCREMENT, \
-        col_file TEXT, \
-        col_type TEXT \
+        col_file TEXT \
         )")
     conn.commit()
 conn.close()
 
-conn = sqlite3.connect('test3.db')
+conn = sqlite3.connect('test2.db')
 
 with conn:
     cur = conn.cursor()
-    cur.execute("INSERT INTO tbl_files(col_file, col_type) VALUES (?, ?)", \
-                ('Hello.txt','text'))
-    cur.execute("INSERT INTO tbl_files(col_file,col_type) VALUES (?,?)", \
-                ('World.txt',"text"))
-    conn.commit()
-conn.close()
 
-fileList = ('information.docx','Hello.txt','myImage.png',\
+    fileList = ('information.docx','Hello.txt','myImage.png',\
            'myMovie.mpg','World.txt','data.pdf','myPhoto.jpg')
 
-for entry in fileList:
-    if entry.endswith(".txt"):
-        print(entry)
+    for entry in fileList:
+        if entry.endswith(".txt"):
+            cur.execute("INSERT INTO tbl_files(col_file) VALUES (?)",  (entry,)) 
+            print(entry)
+    conn.commit()
+conn.close()
