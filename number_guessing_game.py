d9bc52be037cb59289d5d459cf0317285f6e05f8
Cade Wilson <cade25wilson@gmail.com>
2021-04-18 06:30:46 -0600

    Update number_guessing_game.py
    created instance of game, added play again feature

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,21 +1,36 @@
 import random
-random_number = random.randint(1,100)
-win = False
-turns = 0
-attempts = 0
 
-while win == False:
-    user_Guess = input("enter a number between 1 and 100: ")
-    turns +=1
-    if random_number ==int(user_Guess):
-        print("You Won!")
-        print("Number of turns you used: ",turns)
-        win == True
-        break
-    else:
-        if random_number>int(user_Guess):
-            print("Your guess was low please enter a higher number")
+def guessingGame():
+    random_number = random.randint(1,100)
+    win = False
+    turns = 0
+    attempts = 0
+    while win == False:
+        user_Guess = input("enter a number between 1 and 100: ")
+        turns +=1
+        if random_number ==int(user_Guess):
+            print("You Won!")
+            print("Number of turns you used: ",turns)
+            win == True
+            break
         else:
-            print("Your guess was high, please enter a lower number")
+            if random_number>int(user_Guess):
+                print("Your guess was low please enter a higher number")
+            else:
+                print("Your guess was high, please enter a lower number")
             
+def playAgain():
+    user_Answer = input("Would you like to play again? Y = Yes, N = No \n")
+    error_Check = user_Answer.upper()
+    if error_Check == 'Y':
+        guessingGame()
+    elif error_Check =='N':
+        print("thanks for playing!")
+        quit()
+    else:
+        print("Please enter a 'Y' or 'N'")
+        playAgain()
     
+
+guessingGame()
+playAgain()
