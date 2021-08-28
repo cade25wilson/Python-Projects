48efcbdde05eb7569d0385b194b8563c085b74b9
Cade Wilson <cade25wilson@gmail.com>
2021-08-28 17:55:04 -0600

    Update tweepytest.py
    removed personal api key

--- tweepytest.py
+++ tweepytest.py
@@ -1,6 +1,6 @@
 import tweepy
-auth = tweepy.OAuthHandler("N91DlurhYfswAfvqff6z0llf9", "IqdqFjYTHHJdnhsbR3Bj9HdVPLWxMIeGbQMFHJCg65opmqX10d")
-auth.set_access_token("1430602494866907136-Tqtdw8bHROhPz4xIPhsatiq4Jpb6Qv", "s7xiMKDRhOIbgK6lX7rAiBsQt9RR60maqVD3N3SitS1us")
+auth = tweepy.OAuthHandler("(api key)", "(api secret key)")
+auth.set_access_token("(access token)", "(secret access token)")
 api = tweepy.API(auth)
 try:
 	api.verify_credentials()
