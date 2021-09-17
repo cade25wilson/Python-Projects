4376c146a90118845184f9aa09246e1c775c88ec
Cade Wilson <cade25wilson@gmail.com>
2021-09-17 03:22:59 -0600

    added button functionality

--- contactbook/contacts/model.py
+++ contactbook/contacts/model.py
@@ -15,3 +15,23 @@ class ContactsModel:
         for columnIndex, header in enumerate(headers):
             tableModel.setHeaderData(columnIndex, Qt.Horizontal, header)
             return tableModel
+
+    def addContact(self, data):
+        rows = self.model.rowCount()
+        self.model.insertRows(rows, 1)
+        for column, field in enumerate(data):
+            self.model.setData(self.model.index(rows, column + 1), field)
+        self.model.submitAll()
+        self.model.select()
+
+    def deleteContact(self, row):
+        self.model.removeRow(row)
+        self.model.submitAll()
+        self.model.select()
+
+    def clearContact(self):
+        self.model.setEditStrategy(QSqlTableModel.OnManualSubmit)
+        self.model.removerows(0, self.model.rowCount())
+        self.model.submitAll()
+        self.model.setEditStrategy(QSqlTableModel.OnFieldChange)
+        self.model.select()
