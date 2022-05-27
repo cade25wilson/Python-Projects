e8a1efa27170e18ee91aee04bb77d5ba9a6a110d
Cade Wilson <Cade25wilson@gmail.com>
2022-05-27 10:49:15 -0600

    initial creation

--- bugtracker/bugtracker/settings.py
+++ bugtracker/bugtracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'signup',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'bugtracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bugtracker',
+        'USER': 'postgres',
+        'PASSWORD': 'BrightBridge1',
+        'HOST': 'localhost',
+        'PORT': '5433',
     }
 }
 
